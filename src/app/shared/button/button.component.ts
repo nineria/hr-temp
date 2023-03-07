936654d38e9ff8a01b88ed72f95fbecd35f3a887
Shinde kudasai ❤〜 <changlovemom@gmail.com>
2023-03-08 00:22:55 +0700

    feat: refactor button

--- src/app/shared/button/button.component.ts
+++ src/app/shared/button/button.component.ts
@@ -1,11 +1,11 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-button',
   templateUrl: './button.component.html',
   styleUrls: ['./button.component.scss'],
 })
-export class ButtonComponent {
+export class ButtonComponent implements OnInit {
   @Input()
   label: string = '';
 
@@ -22,5 +22,38 @@ export class ButtonComponent {
   variant: string = 'default';
 
   @Input()
-  size: string = 'md';
+  padding: string = '10px 15px';
+
+  @Input()
+  rounded: string = '10px';
+
+  constructor() {}
+
+  ngOnInit(): void {
+    switch (this.padding) {
+      default:
+        this.padding = `padding: ${this.padding};`;
+        break;
+    }
+
+    switch (this.rounded) {
+      case 'sm':
+        this.rounded = 'border-radius: 5px;';
+        break;
+      case 'md':
+        this.rounded = 'border-radius: 10px;';
+
+        break;
+      case 'lg':
+        this.rounded = 'border-radius: 15px;';
+
+        break;
+      case 'full':
+        this.rounded = 'border-radius: 100%;';
+        break;
+      default:
+        this.rounded = `border-radius: ${this.rounded};`;
+        break;
+    }
+  }
 }
