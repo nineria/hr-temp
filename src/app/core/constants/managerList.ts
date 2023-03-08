13b224019ee64fed4d7b97faec0f68e6d72868e3
Shinde kudasai ❤〜 <changlovemom@gmail.com>
2023-03-08 19:19:17 +0700

    feat: update object and interface

--- src/app/core/constants/managerList.ts
+++ src/app/core/constants/managerList.ts
@@ -2,41 +2,49 @@ export const managerList = [
   {
     id: 1,
     name: 'สงวน ลิขสิทธิ์',
+    position: 'CEO',
     avatar: 'https://www.w3schools.com/howto/img_avatar.png',
   },
   {
     id: 2,
     name: 'ณรงค์ นัดใช้ปืน',
+    position: 'CEO',
     avatar: 'https://www.w3schools.com/howto/img_avatar.png',
   },
   {
     id: 3,
     name: 'ศักดิพันธ์ ชอบนอนหงาย',
+    position: 'CEO',
     avatar: 'https://www.w3schools.com/howto/img_avatar.png',
   },
   {
     id: 4,
     name: 'นิธินัย เหินเวหา',
+    position: 'CEO',
     avatar: 'https://www.w3schools.com/howto/img_avatar.png',
   },
   {
     id: 5,
     name: 'ไพรัตน์ หม้อน้ำร้อน',
+    position: 'CEO',
     avatar: 'https://www.w3schools.com/howto/img_avatar.png',
   },
   {
     id: 6,
     name: 'ลำเทียน จ้องผสมพันธุ์',
+    position: 'CEO',
     avatar: 'https://www.w3schools.com/howto/img_avatar.png',
   },
   {
     id: 7,
     name: 'บุญพอ มีเท',
+    position: 'CEO',
     avatar: 'https://www.w3schools.com/howto/img_avatar.png',
   },
   {
     id: 8,
     name: 'บุญศรัทธา มหามงคล',
+    position: 'CEO',
     avatar: 'https://www.w3schools.com/howto/img_avatar.png',
   },
 ];
---
+++ src/app/core/interfaces/managerList.ts
@@ -0,0 +1,7 @@
+export interface ManagerList {
+  id: number;
+  name: string;
+  inputValue?: string;
+  avatar?: string;
+  position: string;
+}
