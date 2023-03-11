4542bb0e844cccecfbd150d67186bb26e388a85a
Shinde kudasai ❤〜 <changlovemom@gmail.com>
2023-03-11 20:52:42 +0700

    feat: tabs component

--- src/app/features/resume/resume-view/tabs/tabs.component.ts
+++ src/app/features/resume/resume-view/tabs/tabs.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-tabs',
   templateUrl: './tabs.component.html',
-  styleUrls: ['./tabs.component.scss']
+  styleUrls: ['./tabs.component.scss'],
 })
 export class TabsComponent {
+  toggle: boolean = true;
 
+  @Input()
+  files: any;
 }
